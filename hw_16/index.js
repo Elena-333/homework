99523ee3ab711558a25d58d93911ad9fb80dad35
Elena Anisina <108744353+Elena-333@users.noreply.github.com>
2022-07-26 01:50:15 +0300

    Add files via upload

---
+++ hw_16/index.js
@@ -0,0 +1,15 @@
+var a = 5;
+alert('Любой текст ' + a);
+var name = prompt("Как вас зовут?");
+console.log("Привет ," + name);
+var name = prompt("Тебе нравится кататься на лыжах?");
+if (name) {
+    console.log("Я люблю кататься на лыжах");
+}
+else {
+    console.log("Я не люблю кататься на лыжах");
+}
+confirm("Are you sure of your choice?");
+var a = 5 + 5;
+console.log(Результат);
+console.log(a);
